ee2b94e48ba1b445474d8223e5af33459734e471
Justin Fagnani <justin@fagnani.com>
2023-05-04 13:40:09 -0700

    Add initial tools utilities (#3)

---
+++ packages/tools/src/lib/resolve-reference.ts
@@ -0,0 +1,95 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import type {
+  Declaration,
+  Module,
+  Package,
+  Reference,
+} from 'custom-elements-manifest/schema.js';
+import {getModule, normalizeModulePath} from './get-module.js';
+
+/**
+ * Resolves a manifest reference from a local package and module into a
+ * Declaration object.
+ *
+ * TODO (justinfagnani): The current implementation can only resolve within the
+ * local package. To resolve to external packages would require some kind of
+ * package registry, which might require this function to be async.
+ *
+ * @param pkg The package to resolve the reference within
+ * @param localModule The module to resolve the reference from
+ * @param ref The reference to resolve
+ * @param packageName The name of the package to use in error messages
+ * @param packageVersion The version of the package to use in error messages
+ */
+export const resolveReference = (
+  pkg: Package,
+  localModule: Module,
+  ref: Reference,
+  packageName: string,
+  packageVersion: string
+): Declaration | undefined => {
+  // Check for local reference
+  if (ref.package !== undefined && ref.package !== packageName) {
+    // We don't know how to resolve cross-package references yet
+    console.warn(
+      `Can't resolve cross-package reference ${ref} (from package ${packageName})`
+    );
+    return undefined;
+  }
+  if (
+    ref.module === undefined ||
+    normalizeModulePath(ref.module) === normalizeModulePath(localModule.path)
+  ) {
+    // Local reference. Local references refer to declarations in the local
+    // module scope, which may or may not be exported.
+    return localModule.declarations?.find((d) => d.name === ref.name);
+  } else {
+    // Cross-module reference. A reference to a different module references
+    // an export or that module.
+    const mod = getModule(pkg, ref.module);
+    if (mod === undefined) {
+      // Module not found
+      const modules = pkg.modules.map((m) => m.path);
+      console.warn(
+        "Can't find module",
+        packageName,
+        packageVersion,
+        ref.module,
+        modules
+      );
+      return undefined;
+    }
+    const exprt = mod.exports?.find((e) => e.name === ref.name);
+    if (exprt === undefined) {
+      console.warn(
+        "Can't find export",
+        packageName,
+        packageVersion,
+        ref.module,
+        ref.name
+      );
+      return undefined;
+    }
+    const declaration = exprt.declaration;
+    // Protect against infinite recursion:
+    if (mod === localModule && declaration == ref) {
+      throw new Error(
+        `Detected cycle in reference: ${JSON.stringify(declaration)}`
+      );
+    }
+    // Recurse to find the declaration with the new module as the local module.
+    // There are two cases:
+    //  - declaration is local to the new module (its module property is
+    //    undefined), in which case the recursion call is to look up a local
+    //    reference to a declaration within the new module.
+    //  - declaration is a reference to another module, (module is defined) in
+    //    which case the recursion call will look for an export of the new
+    //    module, then recurse again to find the declaration.
+    return resolveReference(pkg, mod, declaration, packageName, packageVersion);
+  }
+};
---
+++ packages/tools/src/test/lib/resolve-reference_test.ts
@@ -0,0 +1,177 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+import type {Package} from 'custom-elements-manifest/schema.js';
+import {suite} from 'uvu';
+// eslint-disable-next-line import/extensions
+import * as assert from 'uvu/assert';
+import {resolveReference} from '../../lib/resolve-reference.js';
+
+const test = suite('resolveReference');
+
+const pkg: Package = {
+  schemaVersion: '1.0.0',
+  modules: [
+    {
+      kind: 'javascript-module',
+      path: 'module-1.js',
+      exports: [
+        {kind: 'js', name: 'a', declaration: {name: 'a'}},
+        // A re-export:
+        {
+          kind: 'js',
+          name: 'b',
+          declaration: {name: 'b', module: '/module-2.js'},
+        },
+      ],
+      declarations: [
+        {
+          kind: 'variable',
+          name: 'a',
+        },
+      ],
+    },
+    {
+      kind: 'javascript-module',
+      path: 'module-2.js',
+      exports: [{kind: 'js', name: 'b', declaration: {name: 'b'}}],
+      declarations: [
+        {
+          kind: 'variable',
+          name: 'b',
+        },
+        {
+          kind: 'variable',
+          name: 'c',
+        },
+      ],
+    },
+    {
+      kind: 'javascript-module',
+      path: 'x-foo.js',
+      declarations: [],
+      exports: [
+        {
+          kind: 'custom-element-definition',
+          name: 'x-foo',
+          declaration: {
+            name: 'XFoo',
+            module: '/src/components/x-foo.js',
+          },
+        },
+      ],
+    },
+    {
+      kind: 'javascript-module',
+      path: 'src/components/x-foo.js',
+      declarations: [
+        {
+          kind: 'class',
+          name: 'XFoo',
+          customElement: true,
+        },
+      ],
+      exports: [
+        {
+          kind: 'js',
+          name: 'XFoo',
+          declaration: {
+            name: 'XFoo',
+            module: 'src/components/x-foo.js',
+          },
+        },
+      ],
+    },
+  ],
+};
+
+test('resolves inner-module reference', () => {
+  const module1 = pkg.modules[0]!;
+  const variableA = module1.declarations![0]!;
+  const result = resolveReference(
+    pkg,
+    module1,
+    {name: 'a'},
+    'test-package',
+    '1.0.0'
+  );
+  assert.equal(result, variableA);
+});
+
+test('resolves cross-module export reference', () => {
+  // A reference to an export should resolve to that export's declaration
+  const module1 = pkg.modules[0]!;
+  const module2 = pkg.modules[1]!;
+  const variableB = module2.declarations![0]!;
+  const result = resolveReference(
+    pkg,
+    module1,
+    {name: 'b', module: 'module-2.js'},
+    'test-package',
+    '1.0.0'
+  );
+  assert.equal(result, variableB);
+});
+
+test('cross-module references must be exported', () => {
+  // A reference to an export should resolve to that export's declaration
+  const module1 = pkg.modules[0]!;
+  const result = resolveReference(
+    pkg,
+    module1,
+    {name: 'c', module: 'module-2.js'},
+    'test-package',
+    '1.0.0'
+  );
+  assert.equal(result, undefined);
+});
+
+test('resolves a re-exported reference', () => {
+  const module2 = pkg.modules[1]!;
+  const variableB = module2.declarations![0]!;
+  const result = resolveReference(
+    pkg,
+    // Using module2 so we resolve this from outside of module-1, which is
+    // re-exporting `b`
+    module2,
+    {name: 'b', module: 'module-1.js'},
+    'test-package',
+    '1.0.0'
+  );
+  assert.equal(result, variableB);
+});
+
+test('cross-package references are unsupported', () => {
+  // A reference to an export should resolve to that export's declaration
+  const module1 = pkg.modules[0]!;
+  const result = resolveReference(
+    pkg,
+    module1,
+    {name: 'x', module: 'foo.js', package: 'foo'},
+    'test-package',
+    '1.0.0'
+  );
+  assert.equal(result, undefined);
+});
+
+test(`resolves a custom element defintion to it's declaration`, () => {
+  const definitionModule = pkg.modules.find((m) => m.path === 'x-foo.js')!;
+  const elementExport = definitionModule.exports![0];
+  const declarationReference = elementExport!.declaration;
+  const result = resolveReference(
+    pkg,
+    definitionModule,
+    declarationReference,
+    'test-package',
+    '1.0.0'
+  );
+  console.log('result', result);
+  assert.ok(result);
+});
+
+test.run();
